59e4da1de2702ccec86c0f55b73a2f32e71bdcea
cjahumada <ahumada.cesar@uabc.edu.mx>
2024-05-26 13:22:09 -0700

    Extrae informacion de la base de datos

---
+++ app/get_student.php
@@ -0,0 +1,53 @@
+<?php
+// Permitir solicitudes desde cualquier origen
+header("Access-Control-Allow-Origin: *");
+// Permitir métodos HTTP GET, POST, y OPTIONS
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+// Permitir los encabezados Content-Type y Authorization en las solicitudes CORS
+header('Content-Type: application/json');
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+include("connection.php");
+
+if($_SERVER["REQUEST_METHOD"] == "GET"){
+    //verificar la conexion de la base de datos
+    if($conn->connect_error){
+        die("Error de conexion de la bse de datos" . $conn->connect_error);
+    }
+
+    $query = "Select id, nombre, edad, genero, facultad, email, pass, materia1, materia2, materia3 FROM estudiantes";
+
+    $stmt=$conn->prepare($query);
+    $stmt->execute();
+
+    $result=$stmt->get_result();
+
+    if($result->num_rows >0){
+        $data[] = array();
+        while($row = $result->fetch_assoc()){
+            $data[] = array(
+                "id"=>$row["id"],
+                "nombre"=>$row["nombre"],
+                "edad"=>$row["edad"],
+                "genero"=>$row["genero"],
+                "facultad"=>$row["facultad"],
+                "email"=>$row["email"],
+                "pass"=>$row["pass"],
+                "materia1"=>$row["materia1"],
+                "materia2"=>$row["materia2"],
+                "materia3"=>$row["materia3"]
+            );
+        }
+        echo json_encode(array("success"=>true,"data"=>$data));
+    }else{
+        echo json_encode(array("success"=>false,"error"=>"No se encontraron los datos"));
+    }
+
+    $stmt->close();
+
+}else{
+    echo json_encode(array("success" => false,"error" => "Soliciud no valida"));
+}
+
+$conn->close();
+?>
--- scripts/display.js
+++ scripts/display.js
@@ -1,58 +1,78 @@
-function displayCards(){
+function displayCards(student){
     let card="";
-    for(let i=0; i<students.length; i++){
-//        console.log(students[i]);
-        let student = students[i];
-
-        card+=`
-        <div class='student'>
+        card=`
+        <div id="${student.id}" class='student'>
             <h4> Nombre: ${student.nombre}  <h4/>
-            <label> Edad: ${student.edad} <br> </label>
-            <label> Genero: ${student.genero} <br> </label>
-            <label> Facultad: ${student.facultad} <br> </label>
-            <label> email: ${student.email} <br> </label>
-            <label> Calif. Materia 1: ${student.materia1} <br> </label>
-            <label> Calif. Materia 2: ${student.materia2} <br> </label>
-            <label> Calif. Materia 3: ${student.materia3} </label>
+            <p> Edad: ${student.edad} <br> </p>
+            <p> Genero: ${student.genero} <br> </p>
+            <p> Facultad: ${student.facultad} <br> </p>
+            <p> email: ${student.email} <br> </p>
+            <p> Calif. Materia 1: ${student.materia1} <br> </p>
+            <p> Calif. Materia 2: ${student.materia2} <br> </p>
+            <p> Calif. Materia 3: ${student.materia3} </p>
             <button>Eliminar<buton>
         </div>
         `;
-    }
-     document.getElementById("studentList").innerHTML=card;
+      document.getElementById("studentList").innerHTML+=card;
 }
 
-function displayTable(){
+function displayTable(student){
     let card="";
-    for(let i=0; i<students.length; i++){
-//        console.log(students[i]);
-        let student = students[i];
-
-        card+=`
+        card=`
         <div class='student2'>
             <table>
-            <tr>
-                <th>Nombre</th>
-                <th>Edad</th>
-                <th>Genero</th>
-                <th>Facultad</th>
-                <th>email</th>
-                <th>Calif. Materia 1</th>
-                <th>Calif. Materia 2</th>
-                <th>Calif. Materia 3</th>
-            </tr>
-            <tr>
-                <td> ${student.nombre} </td>
-                <td> ${student.edad} </td>
-                <td> ${student.genero} </td>
-                <td> ${student.facultad} </td>
-                <td> ${student.email} </td>
-                <td> ${student.materia1} </td>
-                <td> ${student.materia2} </td>
-                <td> ${student.materia3} </td>
-            </tr>
+                <tr>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Edad</th>
+                    <th>Genero</th>
+                   <th>Facultad</th>
+                    <th>email</th>
+                    <th>Calif. Materia 1</th>
+                    <th>Calif. Materia 2</th>
+                    <th>Calif. Materia 3</th>
+                </tr>
+                <tr>
+                    <td> ${student.id} </td>
+                    <td> ${student.nombre} </td>
+                    <td> ${student.edad} </td>
+                    <td> ${student.genero} </td>
+                    <td> ${student.facultad} </td>
+                    <td> ${student.email} </td>
+                    <td> ${student.materia1} </td>
+                    <td> ${student.materia2} </td>
+                    <td> ${student.materia3} </td>
+                </tr>
             </table>
         </div>
         `;
-    }
-     document.getElementById("studentList").innerHTML=card;
+    //}
+     document.getElementById("studentList").innerHTML+=card;
 }
+
+function searchToDataBase(){
+    $.ajax({
+        url:"./app/get_student.php",
+        type:"GET",
+        dataType:"json",
+        success:function(response){
+            console.log(response);
+            if (response.success){
+                console.log(response.data);
+                response.data.forEach(displayCards);
+            }else{
+                console.log("Error en la respuesta del servidor");
+            }
+        },
+        error:function(xhr, status, error){
+            console.log(error);
+        }
+    })
+}
+
+
+function init(){
+    searchToDataBase();
+}
+
+window.onload=init;
